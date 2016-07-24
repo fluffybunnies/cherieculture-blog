5a1a2aff104dbc09f82ce902b3fcabc80c6e3e3d
alec <alec.hulce@gmail.com>
2016-07-24 13:56:24 -0700

    （╯°□°）╯︵( .o.)

--- web/wp-content/themes/cherieculture-blog/footer.php
+++ web/wp-content/themes/cherieculture-blog/footer.php
@@ -15,9 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cherieculture-blog' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'cherieculture-blog' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'cherieculture-blog' ), 'cherieculture-blog', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
---
+++ web/wp-content/themes/cherieculture-blog/home.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Overrides index.php for homepage /
+ *
+ * @package cherieculture-blog
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			yo, im the homepage
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+//get_sidebar();
+get_footer();
