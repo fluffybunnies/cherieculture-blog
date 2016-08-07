eb4509fa1eed5f22d602e36740b7303697f5b850
alec <alec.hulce@gmail.com>
2016-08-07 16:11:20 -0700

    DynamicFontSize experimental solution for long post titles on homepage teaser

--- web/wp-content/themes/cherieculture-blog/js/home.js
+++ web/wp-content/themes/cherieculture-blog/js/home.js
@@ -5,6 +5,7 @@
 			defaultOpts: {
 				pagNum: 3
 				,offset: 10
+				,onNewItemsAddedToDom: null
 			}
 		}
 		,init: function($btn,$postToCopy,opts){
@@ -45,16 +46,20 @@
 		}
 		,insertPosts: function(posts){
 			var z = this
+				,$newPosts = $([])
 			$.each(posts,function(i,post){
-				z.insertPost(post)
+				$newPosts = $newPosts.add( z.insertPost(post) )
 			})
+			if (z.opts.onNewItemsAddedToDom)
+				z.opts.onNewItemsAddedToDom($newPosts)
 		}
 		,insertPost: function(post){
 			var z = this
 				,$post = z.$postToCopy.clone()
 				,$timestamp,$img
 			// title
-			$post.find('[x-showmore-key="title"]').text(post.title&&post.title.rendered)
+			//$post.find('[x-showmore-key="title"]').text(post.title&&post.title.rendered) // title.rendered appears to already be escaped safely
+			$post.find('[x-showmore-key="title"]').html(post.title&&post.title.rendered)
 			// timestamp
 			$timestamp = $post.find('[x-showmore-key="timestamp"]')
 			$timestamp.text(chic.util.formatWpTimestamp(post.date_gmt,$timestamp.attr('x-showmore-format')))
@@ -79,13 +84,178 @@
 			// insert
 			z.$postToCopy.after($post)
 			z.$postToCopy = $post
+			return $post
 		}
 	}
 
+	/*
+		To Do
+			- Cross-device testing
+			- Consider putting an invisible fake copy over the top to calculate, then resizing once
+				- Could use css transitions then for a smoother/customizeable font-resize
+				- Problem here is how to share the container without adding to the dom flow and without requiring knowledge of current css definitions
+	*/
+
+	DynamicFontSize.prototype.defaultOpts = {
+		resizeAllToSmallest: true
+		,deltaPx: 0.5
+		,minPx: 8
+		,sensibleMaxResizes: null
+	}
+	DynamicFontSize.prototype.key = 'DynamicFontSize'
+
+	function DynamicFontSize($elms,opts){
+		var z = this
+		z.opts = $.extend({},z.defaultOpts,opts)
+		if (typeof z.opts.sensibleMaxResizes != 'number') z.opts.sensibleMaxResizes = 100/z.opts.deltaPx // in case the DOM is not updating as expected
+
+		z.elms = []
+		$($elms).each(function(){
+			z.elms.push($(this))
+		})
+
+		$(function(){
+			z.domInited = true
+			z.initializeElms()
+			z.fixAll()
+			var resizeSmartTimeout
+			$(window).on('resize',function(){
+				clearTimeout(resizeSmartTimeout)
+				resizeSmartTimeout = setTimeout(function(){
+					z.fixAll()
+				},250)
+			})
+		})
+	}
+
+	// Use this.nextTick() instead of setTimeout(...,0) to group same-thread actions together
+	DynamicFontSize.prototype.nextTickTimeout = null
+	DynamicFontSize.prototype.nextTickActions = []
+	DynamicFontSize.prototype.nextTick = function(cb){
+		var z = this
+		z.nextTickActions.push(cb)
+		z.nextTickTimeout = setTimeout(function(){
+			var actions = z.nextTickActions
+			//console.log(z.key, 'nextTickActions', z.nextTickActions)
+			z.nextTickActions = []
+			$.each(actions,function(i,action){
+				action()
+			})
+		},0)
+	}
+	DynamicFontSize.prototype.resetNexTick = function(){
+		this.nextTickActions = []
+		clearTimeout(this.nextTickTimeout)
+	}
+
+	DynamicFontSize.prototype.initializeElms = function(){
+		var z = this
+		$.each(z.elms,function(i,$elm){
+			$elm.maxFontSize = $elm.css('font-size')
+			if ($elm.maxFontSize.indexOf('px') == -1)
+				console.warn(z.key, 'styled font-size is not in px, may have issues', '@todo: should probly just exit here')
+		})
+	}
+
+	DynamicFontSize.prototype.addItems = function($elms){
+		var z = this
+		$($elms).each(function(){
+			z.elms.push($(this))
+		})
+		if (z.domInited) {
+			z.stopAndResetAll()
+			z.initializeElms()
+			z.fixAll()
+		}
+	}
+
+	DynamicFontSize.prototype.fixAll = function(){
+		var z = this
+			,smallestFontSize
+
+		z.stopAndResetAll()
+
+		$.each(z.elms,function(i,$elm){
+			// DOM needs time to render
+			var n = 0, finalFontSize;
+			(function fixOnce(){
+				z.nextTick(function(){
+					if (++n == z.opts.sensibleMaxResizes) return finishedFixingElm($elm,finalFontSize);
+					var res = z.fix($elm)
+					if (res) {
+						finalFontSize = res
+						fixOnce()
+					} else {
+						finishedFixingElm($elm,finalFontSize)
+					}
+				});
+			}())
+		})
+
+		var numElmsFinished = 0
+		function finishedFixingElm($elm,finalFontSize){
+			if (finalFontSize && (!smallestFontSize || finalFontSize < smallestFontSize[1])) {
+				smallestFontSize = [$elm,finalFontSize]
+			}
+			if (++numElmsFinished == z.elms.length) {
+				//console.log(z.key, 'finished fixing all', 'smallestFontSize=', smallestFontSize)
+				if (z.opts.resizeAllToSmallest && smallestFontSize) {
+					$.each(z.elms,function(i,$elm){
+						$elm.css('font-size',smallestFontSize[1]+'px')
+					})
+				}
+			}
+		}
+	}
+
+	DynamicFontSize.prototype.stopAndResetAll = function(){
+		var z = this
+		z.resetNexTick()
+		$.each(z.elms,function(i,$elm){
+			$elm.lastFontSize = null
+			$elm.css('font-size',$elm.maxFontSize)
+		})
+	}
+
+	DynamicFontSize.prototype.fix = function($elm){
+		// truthy return value means keep going
+		var z = this
+			,fullHeight = $elm[0].scrollHeight
+			,trueHeight = $elm.height() + (parseFloat($elm.css('padding-top'))||0) + (parseFloat($elm.css('padding-bottom'))||0) // omitting border for, not relevant here. and havent even tested to see if scrollHeight includes border xbrowser
+		//console.log(z.key, fullHeight+' <= '+(trueHeight+1), fullHeight <= trueHeight+1)
+		if (fullHeight <= trueHeight+1) {
+			return 0;
+		}
+		var currentFontSize = parseFloat($elm.css('font-size'))
+			,nextFontSize = currentFontSize - z.opts.deltaPx
+		if (isNaN(currentFontSize)) {
+			return console.warn(z.key, 'unable to parse currentFontSize');
+		}
+		if (nextFontSize < z.opts.minPx) {
+			return console.log(z.key, 'currentFontSize reached its minimum value');
+		}
+		if (currentFontSize == $elm.lastFontSize) {
+			return console.warn(z.key, 'currentFontSize == lastFontSize', 'DOM is not updating fast enough, or some other problem');
+		}
+		$elm.lastFontSize = currentFontSize
+		$elm.css('font-size', nextFontSize+'px')
+		//console.log(z.key, currentFontSize+'->'+nextFontSize)
+		return nextFontSize;
+	}
+
+
 	$(function(){
+		var homeStoriesDynamicFontSizer = new DynamicFontSize($('#main .home-stories-post-title'),{
+			resizeAllToSmallest: true
+		})
+
 		ShowMoreStories.init($('#show-more-posts'),$('#show-more-posts-here .home-stories-post:last'),{
 			pagNum: 3
 			,offset: 10
+			,onNewItemsAddedToDom: function($newItems){
+				homeStoriesDynamicFontSizer.addItems($newItems.find('.home-stories-post-title'))
+			}
 		})
+
 	})
 }())
