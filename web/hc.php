91cb36275bdcf6afce4d606f709e1174346b93df
alec <alec.hulce@gmail.com>
2016-08-20 14:50:38 -0700

    healthcheck:: wordpressDb

--- web/hc.php
+++ web/hc.php
@@ -1,3 +1,34 @@
 <?php
-//header('HTTP/1.1 404 Not Found');exit;
-echo "OK";
+if (!defined('WEBROOT'))
+	exit;
+
+use \ace\Ace;
+
+checkWordpressDb();
+
+header('Content-Type: text/plain');
+echo 'OK';
+
+function checkWordpressDb(){
+	try {
+		$db = new mysqli(Ace::getConfig('WORDPRESS_DB_HOST'), Ace::getConfig('WORDPRESS_DB_USER'), Ace::getConfig('WORDPRESS_DB_PASS'), Ace::getConfig('WORDPRESS_DB_NAME'), Ace::getConfig('WORDPRESS_DB_PORT'));
+		if (!empty($db->connect_error))
+			throw new \Exception('Error connecting to DB. Errno: '.$db->connect_errno);
+		$req = $db->query('show tables');
+		if (!$req)
+			throw new \Exception('Error querying DB');
+		$r = $req->fetch_assoc();
+		if (!($r && current($r)))
+			throw new \Exception('Expecting at least one table');
+	} catch (\Exception $e) {
+		errorOut('db', $e->getMessage()."\n".$e->getTraceAsString());
+	}
+}
+
+function errorOut($errorKey, $error){
+	header('HTTP/1.1 503 Service Unavailable');
+	header('X-Error-Key: '.$errorKey);
+	header('Retry-After: 60');
+	echo $error;
+	exit;
+}
